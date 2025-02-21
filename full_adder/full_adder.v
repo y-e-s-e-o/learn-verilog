d487f25c5bd0df4be2d46a621d95dce5e84b7e18
y-e-s-e-o <yeseo011230@gmail.com>
2025-02-22 01:05:42 +0900

    fix: Assign에서 I/O 포트명 수정

--- full_adder/full_adder.v
+++ full_adder/full_adder.v
@@ -2,16 +2,16 @@ module full_adder(
     A_i, B_i, C_i, S_o; C_o
 );
 
-    input A_i;
-    input B_i;
+    input [3:0] A_i;
+    input [3:0] B_i;
     input C_i;
-    output [1:0] F_o;
+    output [3:0] F_o;
 
     wire S_w;
     wire C_w;
 
     assign S_w = A_i ^ B_i ^ C_i;
-    assign C_w = ((A_i ^ B_i) & C) | (A & B);
+    assign C_w = ((A_i ^ B_i) & C_i) | (A_i & B_i);
     // S_w에 저장된 bit와 C_w의 bit 를 붙여서 하나의 출력으로 만들기
     assign F_o = {C_w, S_w}
 
